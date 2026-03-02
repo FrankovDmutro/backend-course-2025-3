f4005238a00dbec4fa24881bee30aa51fe45ce39
FrankovDmutro <dmytro.frankov@lnu.edu.ua>
2026-03-02 18:24:10 +0200

    Виконання першої частини завдання

---
+++ main.js
@@ -0,0 +1,20 @@
+const { program } = require('commander');
+const fs = require('fs');
+
+program
+  .option('-i, --input <path>')
+  .option('-o, --output <path>')
+  .option('-d, --display');
+
+program.parse(process.argv);
+const options = program.opts();
+
+if (!options.input) {
+  console.error("Please, specify input file");
+  process.exit(1);
+}
+
+if (!fs.existsSync(options.input)) {
+  console.error("Cannot find input file");
+  process.exit(1);
+}
