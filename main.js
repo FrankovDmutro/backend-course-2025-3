03c9f811ba7aa43d4c2289966bf68c6b7c074e08
FrankovDmutro <dmytro.frankov@lnu.edu.ua>
2026-03-07 09:36:53 +0200

    Used additional fs commands

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 program
   .option('-i, --input <path>')
   .option('-o, --output <path>')
-  .option('-d, --display');
+  .option('-d, --display')
+  .option('-h, --humidity')
+  .option('-r, --rainfall <value>');
 
 program.parse(process.argv);
 const options = program.opts();
@@ -17,4 +19,29 @@ if (!options.input) {
 if (!fs.existsSync(options.input)) {
   console.error("Cannot find input file");
   process.exit(1);
+}
+
+const rawData = fs.readFileSync(options.input, 'utf8');
+let data = JSON.parse(rawData);
+
+if (options.rainfall) {
+  const minRain = parseFloat(options.rainfall);
+  data = data.filter(item => item.Rainfall > minRain);
+}
+
+const result = data.map(item => {
+  let line = `${item.Rainfall} ${item.Pressure3pm}`;
+  
+  if (options.humidity) {
+    line += ` ${item.Humidity3pm}`;
+  }
+  return line;
+}).join('\n');
+
+if (options.display) {
+  console.log(result);
+}
+
+if (options.output) {
+  fs.writeFileSync(options.output, result, 'utf8');
 }
